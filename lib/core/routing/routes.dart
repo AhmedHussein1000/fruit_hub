ce4838661d9a3e68aef3f3c0c71f44b43e32c3f8
AhmedHussein07 <ahmed.hussein.ah07@gmail.com>
2024-11-13 15:06:10 +0200

    Added app architecture design
    -Added App Router class
    -Added Routes class
    -Added App Colors class
    -Added font styles class
    -Added No Route Defined screen
    -initial splash screen
    -initial onboarding screen
    -initial login screen
    -initial signup screen

---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,6 @@
+abstract class Routes {
+  static const String splash = '/splash';
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String signup = '/signup';
+}
